b0a10a916ff4d915f301c80d789f725291246502
k.sumovskyi <koramon2015@gmail.com>
2021-03-12 20:51:56 +0200

    hotfix: import location

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import indigo from '@material-ui/core/colors/indigo';
+
+import App from "./App";
 
 const theme = createMuiTheme({
   backgroundColor: indigo[100],
